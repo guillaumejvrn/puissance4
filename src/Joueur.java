a4c9e4ab97a902b7b2b2e3cd87a5d8e856b24a9d
guillaumejvrn <guillaume.jenvrin@sts-sio-caen.info>
2024-02-22 15:56:50 +0100

    battle sage

--- src/Joueur.java
+++ src/Joueur.java
@@ -1,2 +1,23 @@
+import java.util.Scanner;
+
 public class Joueur {
+    //creation des attributs
+    private String nom;
+    private String symbole;
+
+    //creation du constructeur
+    public Joueur(String nom, String symbole){
+        this.nom = nom;
+        this.symbole = symbole;
+    }
+
+    //creation des methodes
+    public String getNom(){
+        return this.nom;
+    }
+    public String getSymbole(){
+        return this.symbole;
+    }
+
+
 }
--- src/Puissance4.java
+++ src/Puissance4.java
@@ -1,9 +1,24 @@
+import java.util.Scanner;
+
 public class Puissance4 {
 
     public static void main(String[] args) {
 
-        boolean fin = false;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Entrez le nom du joueur 1 : ");
+        String nom1 = sc.nextLine();
+        System.out.println("Entrez le symbole du joueur 1 : ");
+        String symbole1 = sc.nextLine();
+        Joueur joueur1 = new Joueur(nom1, symbole1);
 
+        System.out.println("Entrez le nom du joueur 2 : ");
+        String nom2 = sc.nextLine();
+        System.out.println("Entrez le symbole du joueur 2 : ");
+        String symbole2 = sc.nextLine();
+        Joueur joueur2 = new Joueur(nom2, symbole2);
+
+
+        boolean fin = false;
 
         while (fin == false){
             for (int i = 0; i < 6; i++) {
