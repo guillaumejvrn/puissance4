c6000f6d9803092479b4e53a2059c287913f0ab2
guillaumejvrn <guillaume.jenvrin@sts-sio-caen.info>
2024-04-15 08:25:15 +0200

    battle sage

--- src/Puissance4.java
+++ src/Puissance4.java
@@ -1,13 +1,5 @@
 public class Puissance4 {
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Line;
-import javafx.stage.Stage;
-
-
     public static void main(String[] args) {
         Grille grille = new Grille(6, 7);
         Joueur joueur1 = new Humain('X');
